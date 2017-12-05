0bf2c96a2ecc8c6317e7fb1c0eda4a7db8f5e603
Mikael Trieb <mikael@smoofit.com>
2017-12-05 14:08:27 +0100

    Update mosquitto setup.

--- mosquitto/run.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-	-p 8883:8883 \
-    -p 9883:9883 \
-    -v /opt/mosquitto:/opt/mosquitto \
-    -v /opt/mosquitto/logs:/var/log/mosquitto \
-    -v /opt/mosquitto/certs/trusted:/etc/mosquitto/certs \
-    --name=mosquitto-encrypted \
-    docker-mosquitto
